46a61072f9c6a1348f96161169449681edfd777b
abishop0894 <82795351+abishop0894@users.noreply.github.com>
2022-03-16 14:39:43 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: "nGQApIZFpNzb6BD6mDPhDe3n8",
+  consumer_secret: "xaxp2cXkFBX04fwJ253bHANLFcCvvLCkDf295grXUbS0FH449r",
+  access_token_key: "1499981107328077825-jhbk1ARV60it9DGNFgZlDXlP6zHHSy",
+  access_token_secret: "FGebf5bF85gAuVyuKfP8jaNDSYHSs9VWNgfAYxgiyvGUP",
+};
